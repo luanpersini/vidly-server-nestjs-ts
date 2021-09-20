1162859f1801bb5eaf64210b1434757a05d99c7e
Luan Persini <luanpersini@hotmail.com>
2021-09-20 18:37:34 -0300

    feat: add genres using nestjs crud generator

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { Module } from '@nestjs/common'
+import { GenresModule } from './genres/genres.module';
 
 @Module({
-  imports: [],
+  imports: [GenresModule],
   controllers: [AppController],
   providers: [AppService]
 })
