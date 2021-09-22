75a82f6d8b00869ea08173c2dc9833910cee3354
Luan Persini <luanpersini@hotmail.com>
2021-09-21 23:44:03 -0300

    chore: add env.ts file and whitelist configuration to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { AppModule } from './app.module'
 import { NestFactory } from '@nestjs/core'
-import env from './common/config/env'
+import { ValidationPipe } from '@nestjs/common'
+import env from './config/env'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  )
   await app.listen(env.port)
 }
 bootstrap()
