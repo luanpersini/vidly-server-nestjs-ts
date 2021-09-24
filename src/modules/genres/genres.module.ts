86f275e8f5309fe6948930f1face42a81fddf5c0
Luan Persini <luanpersini@hotmail.com>
2021-09-24 00:01:03 -0300

    chore: add sequelize to genres.module

--- src/modules/genres/genres.module.ts
+++ src/modules/genres/genres.module.ts
@@ -1,19 +1,13 @@
+import { Genre } from './domain/genre.model'
 import { GenresController } from './genres.controller'
 import { GenresRepository } from './genres.repository'
 import { GenresService } from './genres.service'
 import { Module } from '@nestjs/common'
+import { SequelizeModule } from '@nestjs/sequelize'
 
 @Module({
+  imports: [SequelizeModule.forFeature([Genre])],
   controllers: [GenresController],
-  providers: [
-    {
-      provide: 'GenresServiceInterface',
-      useClass: GenresService
-    },
-    {
-      provide: 'GenresRepositoryInterface',
-      useClass: GenresRepository
-    }
-  ]
+  providers: [GenresRepository, GenresService]
 })
 export class GenresModule {}
