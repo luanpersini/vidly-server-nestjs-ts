aefd7d7788cb7d63e66e9c8b32880d91d288b74d
Luan Persini <luanpersini@hotmail.com>
2021-09-24 21:07:53 -0300

    feat: add update business rules

--- src/modules/genres/genres.controller.ts
+++ src/modules/genres/genres.controller.ts
@@ -11,7 +11,6 @@ import {
   Post
 } from '@nestjs/common'
 import { ItemNotFoundError } from 'src/common/errors/item-not-found-error'
-import { Messages } from 'src/common/standard-messages/standard.messages'
 import { GenreDto } from './domain/genre.dto'
 import { GenresService } from './genres.service'
 
@@ -48,7 +47,19 @@ export class GenresController {
 
   @Patch(':id')
   async update(@Param('id') id: string, @Body() body: GenreDto) {
-    return await this.genresService.update(id, body)
+    try {
+      const genreExists = await this.genresService.findOneByName(body.name)
+      if (genreExists && genreExists.id !== id) {
+        throw new BadRequestException('A genre with the given name already exists')
+      }
+      const genre = await this.genresService.update(id, body)
+      if (!genre) {
+        throw new NotFoundException(new ItemNotFoundError('genre'))
+      }
+      return genre
+    } catch (error) {
+      throw new InternalServerErrorException(error)
+    }
   }
 
   @Delete(':id')
@@ -58,7 +69,6 @@ export class GenresController {
       if (deleted === 0) {
         throw new NotFoundException(new ItemNotFoundError('genre'))
       }
-      return Messages.deleteSuccess('Genre')
     } catch (error) {
       throw new InternalServerErrorException(error)
     }
