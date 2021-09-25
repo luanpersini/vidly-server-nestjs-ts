e024bee1372b40d4d93e2975e34e42ba658de871
Luan Persini <luanpersini@hotmail.com>
2021-09-24 21:06:08 -0300

    feat: add findOneByName in genres.repository

--- src/modules/genres/genres.repository.ts
+++ src/modules/genres/genres.repository.ts
@@ -1,11 +1,19 @@
-import { Injectable } from '@nestjs/common'
 import { BaseRepositoryAbstract } from 'src/common/database/repositories/base.repository.abstract'
-import { GenreDto } from './domain/genre.dto'
 import { Genre } from './domain/genre.model'
+import { GenreDto } from './domain/genre.dto'
+import { Injectable } from '@nestjs/common'
 
 @Injectable()
 export class GenresRepository extends BaseRepositoryAbstract<Genre, string, GenreDto, GenreDto> {
   constructor() {
     super(Genre)
+  }
+
+  async findOneByName(name: string): Promise<Genre> {
+    return await Genre.findOne({
+      where: {
+        name
+      }
+    })
   }
 }
