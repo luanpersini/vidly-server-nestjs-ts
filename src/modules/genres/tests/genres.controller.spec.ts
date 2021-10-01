3e5ebd4700094ff03b0bdc058402be52da30e46c
Luan Persini <luanpersini@hotmail.com>
2021-10-01 16:29:43 -0300

    test: add genresControler.create tests

---
+++ src/modules/genres/tests/genres.controller.spec.ts
@@ -0,0 +1,64 @@
+import { ArgumentMetadata, BadRequestException, ValidationPipe } from '@nestjs/common'
+import { Test, TestingModule } from '@nestjs/testing'
+import * as faker from 'faker'
+import { ItemAlreadyExistsError } from 'src/common/errors/item-already-exists-error'
+import { GenreDto } from '../domain/genre.dto'
+import { Genre } from '../domain/genre.model'
+import { GenresController } from '../genres.controller'
+import { GenresService } from '../genres.service'
+
+const genreName = faker.music.genre()
+const createDto = { name: genreName } as GenreDto
+const fakeGenre = { id: faker.datatype.uuid(), name: genreName } as Genre
+
+describe('GenresController', () => {
+  let genresController: GenresController
+  let genresService: GenresService
+
+  beforeEach(async () => {
+    const GenresServiceProvider = {
+      provide: GenresService,
+      useFactory: () => ({
+        create: jest.fn(() => fakeGenre),
+        findOne: jest.fn(() => fakeGenre)
+      })
+    }
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [GenresController],
+      providers: [GenresService, GenresServiceProvider]
+    }).compile()
+
+    genresController = module.get<GenresController>(GenresController)
+    genresService = module.get<GenresService>(GenresService)
+  })
+
+  describe('genresController.create', () => {
+    it('should call genresService.create with the correct values', () => {
+      expect(genresController).toBeDefined()
+      expect(genresController.create(createDto)).not.toEqual(null)
+    })
+    test('should Return **Forbidden** if there is already an genre with the given name', async () => {
+      jest.spyOn(genresService, 'create').mockReturnValueOnce(null)
+      const httpResponse = await genresController.create(createDto)
+      expect(httpResponse).toEqual(
+        new BadRequestException(new ItemAlreadyExistsError('Genre', 'Name'))
+      )
+    })
+    test('should Return **BadRequest** if validation returns an error', async () => {
+      const target: ValidationPipe = new ValidationPipe({ transform: true, whitelist: true })
+      const metadata: ArgumentMetadata = {
+        type: 'body',
+        metatype: GenreDto,
+        data: ''
+      }
+      await target.transform(<GenreDto>{}, metadata).catch((err) => {
+        expect(err.getResponse().statusCode).toEqual(400)
+      })
+    })
+    test('should Return **Ok** with the created genre data', async () => {
+      const httpResponse = await genresController.create(createDto)
+      expect(httpResponse).toEqual(fakeGenre)
+    })
+  })
+})
