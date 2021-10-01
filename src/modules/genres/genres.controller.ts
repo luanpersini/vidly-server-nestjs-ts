6265cb17d2ba6c0d307efda45e2f0d9454ad9595
Luan Persini <luanpersini@hotmail.com>
2021-10-01 00:16:27 -0300

    feat: add ParseUUIDPipe to controllers

--- src/modules/genres/genres.controller.ts
+++ src/modules/genres/genres.controller.ts
@@ -4,12 +4,13 @@ import {
   Controller,
   Delete,
   Get,
-  InternalServerErrorException,
   NotFoundException,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post
 } from '@nestjs/common'
+import { ItemAlreadyExistsError } from 'src/common/errors/item-already-exists-error'
 import { ItemNotFoundError } from 'src/common/errors/item-not-found-error'
 import { GenreDto } from './domain/genre.dto'
 import { GenresService } from './genres.service'
@@ -22,7 +23,7 @@ export class GenresController {
   async create(@Body() body: GenreDto) {
     const genre = await this.genresService.create(body)
     if (!genre) {
-      throw new BadRequestException('A genre with the given name already exists')
+      throw new BadRequestException(new ItemAlreadyExistsError('Genre', 'Name'))
     }
     return genre
   }
@@ -33,44 +34,32 @@ export class GenresController {
   }
 
   @Get(':id')
-  async findOne(@Param('id') id: string) {
-    try {
-      const genre = await this.genresService.findOneById(id)
-      if (!genre) {
-        throw new NotFoundException(new ItemNotFoundError('genre'))
-      }
-      return genre
-    } catch (error) {
-      throw new InternalServerErrorException(error)
+  async findOne(@Param('id', ParseUUIDPipe) id: string) {
+    const genre = await this.genresService.findOneById(id)
+    if (!genre) {
+      throw new NotFoundException(new ItemNotFoundError('Genre'))
     }
+    return genre
   }
 
   @Patch(':id')
-  async update(@Param('id') id: string, @Body() body: GenreDto) {
-    try {
-      const genreExists = await this.genresService.findOneByName(body.name)
-      if (genreExists && genreExists.id !== id) {
-        throw new BadRequestException('A genre with the given name already exists')
-      }
-      const genre = await this.genresService.update(id, body)
-      if (!genre) {
-        throw new NotFoundException(new ItemNotFoundError('genre'))
-      }
-      return genre
-    } catch (error) {
-      throw new InternalServerErrorException(error)
+  async update(@Param('id', ParseUUIDPipe) id: string, @Body() body: GenreDto) {
+    const genreExists = await this.genresService.findOneByName(body.name)
+    if (genreExists && genreExists.id !== id) {
+      throw new BadRequestException('A genre with the given name already exists')
     }
+    const genre = await this.genresService.update(id, body)
+    if (!genre) {
+      throw new NotFoundException(new ItemNotFoundError('Genre'))
+    }
+    return genre
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
-    try {
-      const deleted = await this.genresService.remove(id)
-      if (deleted === 0) {
-        throw new NotFoundException(new ItemNotFoundError('genre'))
-      }
-    } catch (error) {
-      throw new InternalServerErrorException(error)
+  async remove(@Param('id', ParseUUIDPipe) id: string) {
+    const deleted = await this.genresService.remove(id)
+    if (deleted === 0) {
+      throw new NotFoundException(new ItemNotFoundError('Genre'))
     }
   }
 }
