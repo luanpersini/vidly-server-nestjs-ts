a239ab7c7253e3c9d80a17eb0746e24d359aabeb
Luan Persini <luanpersini@hotmail.com>
2021-10-03 15:33:57 -0300

    test: completed genres controller tests

--- src/modules/genres/tests/genres.controller.spec.ts
+++ src/modules/genres/tests/genres.controller.spec.ts
@@ -2,14 +2,16 @@ import { ArgumentMetadata, BadRequestException, ValidationPipe } from '@nestjs/c
 import { Test, TestingModule } from '@nestjs/testing'
 import * as faker from 'faker'
 import { ItemAlreadyExistsError } from 'src/common/errors/item-already-exists-error'
+import { ItemNotFoundError } from '../../../common/errors/item-not-found-error'
 import { GenreDto } from '../domain/genre.dto'
 import { Genre } from '../domain/genre.model'
 import { GenresController } from '../genres.controller'
 import { GenresService } from '../genres.service'
 
 const genreName = faker.music.genre()
+const genreId = faker.datatype.uuid()
 const createDto = { name: genreName } as GenreDto
-const fakeGenre = { id: faker.datatype.uuid(), name: genreName } as Genre
+const fakeGenre = { id: genreId, name: genreName } as Genre
 
 describe('GenresController', () => {
   let genresController: GenresController
@@ -20,7 +22,8 @@ describe('GenresController', () => {
       provide: GenresService,
       useFactory: () => ({
         create: jest.fn(() => fakeGenre),
-        findOne: jest.fn(() => fakeGenre)
+        findAll: jest.fn(() => [fakeGenre]),
+        remove: jest.fn(() => 1)
       })
     }
 
@@ -39,7 +42,7 @@ describe('GenresController', () => {
       expect(genresService.create).toHaveBeenCalledWith(createDto)
     })
 
-    test('should Return **Forbidden** if there is already an genre with the given name', async () => {
+    test('should Return **BadRequest** if there is already an genre with the given name', async () => {
       jest.spyOn(genresService, 'create').mockReturnValueOnce(null)
       const response = await genresController.create(createDto)
       expect(response).toEqual(new BadRequestException(new ItemAlreadyExistsError('Genre', 'Name')))
@@ -58,6 +61,24 @@ describe('GenresController', () => {
     test('should Return **Ok** with the created genre data', async () => {
       const response = await genresController.create(createDto)
       expect(response).toEqual(fakeGenre)
+    })
+  })
+  describe('genresController.findAll', () => {
+    test('should Return **Ok** with the located genres data', async () => {
+      const response = await genresController.findAll()
+      expect(genresService.findAll).toHaveBeenCalledTimes(1)
+      expect(response).toEqual([fakeGenre])
+    })
+  })
+  describe('genresController.remove', () => {
+    test('should call genresService.remove with the correct values', async () => {
+      await genresController.remove(genreId)
+      expect(genresService.remove).toHaveBeenCalledWith(genreId)
+    })
+    test('should Return **NotFound** if no genre with the given ID is found', async () => {
+      jest.spyOn(genresService, 'remove').mockReturnValueOnce(Promise.resolve(0))
+      const response = await genresController.remove(genreId)
+      expect(response).toEqual(new BadRequestException(new ItemNotFoundError('Genre')))
     })
   })
 })
