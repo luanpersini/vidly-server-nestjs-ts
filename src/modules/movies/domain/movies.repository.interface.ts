60e721f03d77193536fb02f5cafe9c7ceb62db36
Luan Persini <luanpersini@hotmail.com>
2022-01-25 21:34:38 -0300

    refactor: remove unused files

--- src/modules/movies/domain/movies.repository.interface.ts
+++
@@ -1,7 +0,0 @@
-import { Genre } from './movie.model'
-import { GenreDto } from './movie.dto'
-
-export interface GenresRepositoryInterface {
-  findAll: (dto: GenreDto) => Promise<Genre[]>
-}
-// export type GenresRepositoryInterface = BaseRepositoryInterface<Genre, GenreDto>
